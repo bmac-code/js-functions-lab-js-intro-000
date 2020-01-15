55d7af48eedb49f78c0baf1b864d6402cc6f2a61
B Mac <bjgamc@gmail.com>
2020-01-15 20:38:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays(string){
+return("Happy holidays!")
+}
+
+function happyHolidaysTo(firstName) {
+return `${Happy holidays}, ${firstName}!`)
+}
+function happyHolidaysTo(holiday, firstName) {
+ return (`Happy holiday}, ${firstName}!`)
+}
